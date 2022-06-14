4c51bee93a272ecdbb7111ed07ef6b5da685ad75
Sagar Kunwar <sagar@Sagars-MacBook-Air.local>
2022-06-14 11:07:14 -0500

    removed unwanted comment

--- PHP-Backend/_multer.js
+++ PHP-Backend/_multer.js
@@ -1,12 +1,8 @@
 const multer = require("multer");
 const storage  = multer.diskStorage({
-    // dest:"public/uploads/",
     destination: function (req, file, cb) {
         cb(null, 'public/uploads/')
       },
-      // filename(req, file = {}, cb) {
-      //   cb(null, file.originalname);
-      // },
       filename(req, file = {}, cb) {
         const { originalname } = file;
         const fileExtension = (originalname.match(/\.+[\S]+$/) || [])[0];
