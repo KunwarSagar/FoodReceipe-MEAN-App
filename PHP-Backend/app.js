cca3e359394691b3c64bcbf2e15ad66afbfecf83
Sagar Kunwar <sagar@Sagars-MacBook-Air.local>
2022-06-15 13:24:06 -0500

    minor fix

--- PHP-Backend/app.js
+++ PHP-Backend/app.js
@@ -15,8 +15,8 @@ app.use(function(req, res, next){
 
 app.use("/api", function(req, res, next){
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header('Access-Control-Allow-Headers', 'Origin, XRequested-With, Content-Type, Accept');
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
 
