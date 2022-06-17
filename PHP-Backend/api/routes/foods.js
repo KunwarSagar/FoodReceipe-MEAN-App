9084564286b3c4314bac044f9b7bef5e96c520ce
Sagar Kunwar <sagar@Sagars-MacBook-Air.local>
2022-06-17 16:02:56 -0500

    authentication added to routes

--- PHP-Backend/api/routes/foods.js
+++ PHP-Backend/api/routes/foods.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
 const foodController = require("../controllers/foods.controller");
+const authController = require("../controllers/auth.controller");
 
 router.route('')
     .get(foodController.getAll)
-    .post(foodController.addOne);
+    .post(authController.authenticate, foodController.addOne);
 
 router.route('/size')
     .get(foodController.getSize);
 
 router.route('/:foodId')
     .get(foodController.getOne)
-    .put(foodController.fullUpdateFood)
-    .patch(foodController.partialUpdateFood)
-    .delete(foodController.deleteone);
+    .put(authController.authenticate, foodController.fullUpdateFood)
+    .patch(authController.authenticate, foodController.partialUpdateFood)
+    .delete(authController.authenticate, foodController.deleteone);
 
 module.exports = router;
--- PHP-Backend/app.js
+++ PHP-Backend/app.js
@@ -11,13 +11,13 @@ const uploads = multer({storage:storage});
 
 app.use("/api", function(req, res, next){
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
 
 app.use(express.urlencoded({extended:true}));
-app.use(express.json()) //10 mb
+app.use(express.json())
 app.use(uploads.single('thumbnailImage'));
 
 app.use("/api/public", express.static(path.join(__dirname, "public")));
