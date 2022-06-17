145fed7d97e9d4123c1a4045699706f4c2372bd9
Sagar Kunwar <sagar@Sagars-MacBook-Air.local>
2022-06-17 11:11:17 -0500

    state managed on login and logout

--- PHP-FrontEnd/src/app/nav/nav.component.ts
+++ PHP-FrontEnd/src/app/nav/nav.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from '../auth.service';
+import { TokenService } from '../token.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +11,50 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router: Router, private authService: AuthService, private tokenService: TokenService) { }
+
+  isLoggedIn!: boolean;
+  name!: string;
 
   ngOnInit(): void {
+    //set state when page is reloaded
+    this.isLoggedIn = this.authService.isLoggedIn();
+    //set state on event
+    this.authService.loggedInStateChanged().subscribe((loggedIn: boolean) => {
+      this.isLoggedIn = loggedIn;
+      this.setName();
+    });
+    
+    this.setName();
   }
-  goToFoods(){
-    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() =>
-    this.router.navigate(['/foods'])); 
+
+  setName() {
+    if (this.isLoggedIn) {
+      let jwtService = new JwtHelperService();
+      this.tokenService.jwtService = jwtService;
+      this.name = this.tokenService.name;
+    }else{
+      this.name = "";
+    }
+  }
+
+  goToFoods(): void {
+    this.router.navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigate(['/foods']));
+  }
+
+  logout(): void {
+    if (this.authService.logout()) {
+      this.setName();
+      this.gotoLogin();
+      return;
+    } else {
+      console.log("logout error");
+    }
+  }
+  gotoLogin(): void {
+    this.router.navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigate(['/login']));
   }
 }
+
